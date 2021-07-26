50d8acf818d604761e7343212a118e5cd5bc18e4
fujun <fujunchn@qq.com>
2021-07-26 17:27:54 +0800

    feat: 添加自动生成文档

---
+++ .vuepress/vuepress.config.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: jun.fu<fujunchn@qq.com>
+ * @LastEditors: jun.fu<fujunchn@qq.com>
+ * @Description: file content
+ * @Date: 2021-07-26 17:01:20
+ * @LastEditTime: 2021-07-26 17:18:42
+ * @FilePath: /winning-his-concepts/.vuepress/vuepress.config.ts
+ */
+module.exports = {
+  plugins: [['@vuepress/plugin-search']],
+  dest: 'dist',
+  themeConfig: {},
+};
