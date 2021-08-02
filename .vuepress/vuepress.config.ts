bda22c22ba8ec54b2966ecf2036e222bb7b90aa6
fujun <fujunchn@qq.com>
2021-08-02 14:42:14 +0800

    build(vuepress): 添加base路径

--- .vuepress/vuepress.config.ts
+++ .vuepress/vuepress.config.ts
@@ -3,10 +3,11 @@
  * @LastEditors: jun.fu<fujunchn@qq.com>
  * @Description: file content
  * @Date: 2021-07-26 17:01:20
- * @LastEditTime: 2021-07-26 17:18:42
+ * @LastEditTime: 2021-08-02 14:41:30
  * @FilePath: /winning-his-concepts/.vuepress/vuepress.config.ts
  */
 module.exports = {
+  base:"/winning-his-concepts/",
   plugins: [['@vuepress/plugin-search']],
   dest: 'dist',
   themeConfig: {},
